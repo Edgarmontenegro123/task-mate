5e4193b4e9e8128defb03efc88dcb3f2ed44fe0f
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-17 02:19:23 -0300

    🕶️ Add icon to navBar component

--- app/components/navBar/navBar.tsx
+++ app/components/navBar/navBar.tsx
@@ -2,7 +2,7 @@ const NavBar = () => {
     return (
         <header className="bg-blue-500 text-white p-4 z-50 fixed top-0 left-0 w-full">
             <nav className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold">TaskMate</h1>
+                <h1 className="text-2xl font-bold"><i className="fa-solid fa-list-check"></i> TaskMate</h1>
                 <ul className="flex space-x-4">
                     <li><a href="/" className="hover:underline">Home</a></li>
                     <li><a href="/about" className="hover:underline">About</a></li>
